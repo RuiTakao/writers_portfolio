4439afa5ca6e5fe9c5c8e7fee001be0947346afb
RuiTakao <ruia1082halfnc@gmail.com>
2023-11-18 22:36:00 +0900

    テスト用コントローラー作成

---
+++ src/Controller/Admin/CreateUsersController.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Admin;
+
+use App\Controller\Admin\AppController;
+
+/**
+ * CreateUsers Controller
+ *
+ * @method \App\Model\Entity\CreateUser[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
+ */
+class CreateUsersController extends AppController
+{
+    /**
+     * Index method
+     *
+     * @return \Cake\Http\Response|null|void Renders view
+     */
+    public function index()
+    {
+    }
+}
---
+++ src/Controller/Admin/TestsController.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Admin;
+
+use App\Controller\Admin\AppController;
+
+/**
+ * Tests Controller
+ *
+ * @method \App\Model\Entity\Test[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
+ */
+class TestsController extends AppController
+{
+    /**
+     * Index method
+     *
+     * @return \Cake\Http\Response|null|void Renders view
+     */
+    public function index()
+    {
+    }
+}
