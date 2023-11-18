ef57bd574dd84d819ee6ee54bcde182d799dcf3d
RuiTakao <ruia1082halfnc@gmail.com>
2023-11-18 21:36:15 +0900

    ユーザーコントローラー作成

---
+++ src/Controller/Admin/UsersController.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller\Admin;
+
+use App\Controller\Admin\AppController;
+
+/**
+ * Users Controller
+ *
+ * @method \App\Model\Entity\User[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
+ */
+class UsersController extends AppController
+{
+    public function login()
+    {
+        
+    }
+}
