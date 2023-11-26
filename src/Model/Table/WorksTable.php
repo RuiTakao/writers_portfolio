00fe6f224c9f5a05b5d7463da7146c0aa5670d96
RuiTakao <ruia1082halfnc@gmail.com>
2023-11-26 11:45:28 +0900

    実績追加実装

---
+++ src/Model/Table/WorksTable.php
@@ -0,0 +1,149 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Works Model
+ *
+ * @property \App\Model\Table\UsersTable&\Cake\ORM\Association\BelongsTo $Users
+ *
+ * @method \App\Model\Entity\Work newEmptyEntity()
+ * @method \App\Model\Entity\Work newEntity(array $data, array $options = [])
+ * @method \App\Model\Entity\Work[] newEntities(array $data, array $options = [])
+ * @method \App\Model\Entity\Work get($primaryKey, $options = [])
+ * @method \App\Model\Entity\Work findOrCreate($search, ?callable $callback = null, $options = [])
+ * @method \App\Model\Entity\Work patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+ * @method \App\Model\Entity\Work[] patchEntities(iterable $entities, array $data, array $options = [])
+ * @method \App\Model\Entity\Work|false save(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Work saveOrFail(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Work[]|\Cake\Datasource\ResultSetInterface|false saveMany(iterable $entities, $options = [])
+ * @method \App\Model\Entity\Work[]|\Cake\Datasource\ResultSetInterface saveManyOrFail(iterable $entities, $options = [])
+ * @method \App\Model\Entity\Work[]|\Cake\Datasource\ResultSetInterface|false deleteMany(iterable $entities, $options = [])
+ * @method \App\Model\Entity\Work[]|\Cake\Datasource\ResultSetInterface deleteManyOrFail(iterable $entities, $options = [])
+ *
+ * @mixin \Cake\ORM\Behavior\TimestampBehavior
+ */
+class WorksTable extends Table
+{
+
+    /**
+     * メッセージ
+     */
+    // 成功時
+    const SUCCESS_ADD_WORKS_MESSAGE = '実績を追加しました。';
+    const SUCCESS_EDIT_WORKS_MESSAGE = '実績の内容を変更しました。';
+    const SUCCESS_EDIT_WORKS_IMAGE_MESSAGE = '実績の画像を変更しました。';
+    const SUCCESS_DELETE_WORKS_MESSAGE = '実績を削除しました。';
+    const SUCCESS_DELETE_WORKS_IMAGE_MESSAGE = '実績の画像を削除しました。';
+    // 失敗時
+
+    // バリデーションメッセージ
+    const INVALID_INPUT_MESSEGE = '入力に不備があります。';
+    const INVALID_EXTENSION_MESSAGE = '拡張子が無効です。';
+
+    /**
+     * 画像ファイルパス
+     */
+    // 画像表示用のパス
+    const WORKS_IMAGE_PATH = 'users/works/';
+    // ルートからの相対パス
+    const ROOT_WORKS_IMAGE_PATH = WWW_ROOT . 'img/' . self::WORKS_IMAGE_PATH;
+
+    /**
+     * 画像の拡張子
+     */
+    const EXTENTIONS = [
+        'jpg',
+        'png',
+        'jpeg',
+        'webp'
+    ];
+
+
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config): void
+    {
+        parent::initialize($config);
+
+        $this->setTable('works');
+        $this->setDisplayField('title');
+        $this->setPrimaryKey('id');
+
+        $this->addBehavior('Timestamp');
+
+        $this->belongsTo('Users', [
+            'foreignKey' => 'user_id',
+            'joinType' => 'INNER',
+        ]);
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator): Validator
+    {
+        $validator
+            ->scalar('title')
+            ->maxLength('title', 50)
+            ->allowEmptyString('title');
+
+        $validator
+            ->scalar('overview')
+            ->maxLength('overview', 255)
+            ->allowEmptyString('overview');
+
+        $validator
+            ->scalar('image_path')
+            ->maxLength('image_path', 255)
+            ->allowEmptyFile('image_path');
+
+        $validator
+            ->scalar('url')
+            ->maxLength('url', 255)
+            ->allowEmptyString('url');
+
+        $validator
+            ->scalar('url_path')
+            ->maxLength('url_path', 255)
+            ->allowEmptyString('url_path');
+
+        $validator
+            ->scalar('works_order')
+            ->maxLength('works_order', 5)
+            ->allowEmptyString('works_order');
+
+        $validator
+            ->integer('user_id')
+            ->notEmptyString('user_id');
+
+        return $validator;
+    }
+
+    /**
+     * Returns a rules checker object that will be used for validating
+     * application integrity.
+     *
+     * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
+     * @return \Cake\ORM\RulesChecker
+     */
+    public function buildRules(RulesChecker $rules): RulesChecker
+    {
+        $rules->add($rules->existsIn('user_id', 'Users'), ['errorField' => 'user_id']);
+
+        return $rules;
+    }
+}
---
+++ templates/Admin/Works/index.php
@@ -0,0 +1,58 @@
+<?php
+
+use App\Model\Table\WorksTable;
+?>
+<?php /** ページタイトル */ ?>
+<?php $this->start('page_title') ?>
+実績
+<?php $this->end() ?>
+
+<?php /** css */ ?>
+<?php $this->start('css') ?>
+<?= $this->Html->css('admin/works') ?>
+<?php $this->end() ?>
+
+<div class="flex" style="gap: 16px">
+    <?= $this->Html->link('実績の追加', ['action' => 'add'], ['class' => 'button']) ?>
+    <?= $this->Html->link('並び順変更', ['action' => 'add'], ['class' => 'button']) ?>
+</div>
+
+<ul style="margin-top: 32px;">
+    <?php foreach ($works as $item) : ?>
+        
+        <?php
+        /**
+         * 画像パス
+         */
+        // 各々のユーザーによって決まるパス
+        $self_path = $auth->username . '/' . $item->id . '/' . $item->image_path;
+        // ルートからのパス
+        $root_image_path = WorksTable::ROOT_WORKS_IMAGE_PATH . $self_path;
+        // webrootからのパス
+        $image_path = WorksTable::WORKS_IMAGE_PATH . $self_path;
+        ?>
+
+        <li class="card">
+            <div class="flex">
+                <div class="flex-left" style="width: 70%;">
+                    <div class="flex">
+                        <p style="font-weight: 600;"><?= h($item->title) ?><?= $this->Html->link('詳細', ['action' => 'detail', $item->id], ['class' => 'button list-button']) ?></p>
+                    </div>
+                    <p style="margin-top: 32px; line-height:1.8em;"><?= nl2br(h($item->overview)) ?></p>
+                </div>
+
+                <?php if (!is_null($item->image_path) && $item->image_path != '' && file_exists($root_image_path)) : ?>
+                    <div class="flex-right" style="width: 30%;">
+                        <div class="">
+                            <?= $this->Html->image($image_path, ['style' => 'width: 80%; height:auto; display: block;']) ?>
+                        </div>
+                        <p style="margin-top: 8px;">
+                            <?= $this->html->link($item->url, ['url' => '#']) ?>
+                        </p>
+                    </div>
+                <?php endif; ?>
+
+            </div>
+        </li>
+    <?php endforeach; ?>
+</ul>
