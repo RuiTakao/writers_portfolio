7b53ee13ba671a296cc0d9656657f0726f044845
RuiTakao <ruia1082halfnc@gmail.com>
2024-01-01 10:22:57 +0900

    ogp画像パス変更

--- templates/LandingPages/index.php
+++ templates/LandingPages/index.php
@@ -10,7 +10,7 @@
     <meta property="og:title" content="Smart Profile - プロフィールサイト作成ツールです。">
     <meta property="og:description" content="簡単にプロフィールサイトを作成できるWebツールです。" />
     <meta property="og:site_name" content="Smart Profile" />
-    <meta property="og:image" content="https://s-profile.jp/favicon.ico" />
+    <meta property="og:image" content="https://s-profile.jp/sp-logo.png" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@400;500;700&family=Playfair+Display:wght@600&display=swap" rel="stylesheet">
